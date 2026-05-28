5b3c87ff52fb33c29647e13eb79a4427a9c10ddb
Joao Gonzales <gonzales.jpgc@gmail.com>
2026-05-28 15:58:44 -0300

    fix: adicionando dependencia vite

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+import json
 
 app = FastAPI()
 
+# libera acesso do frontend
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -12,31 +13,34 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# modelo de login
-class Login(BaseModel):
-    carteirinha: int
-    senha: str
-
-# rota inicial
 @app.get("/")
 def home():
+    return {"mensagem": "Backend funcionando"}
+
+@app.post("/cadastro")
+def cadastro(dados: dict):
+
+    with open("usuarios.json", "r", encoding='utf-8') as arquivo:
+        usuarios = json.load(arquivo)
+
+    carteirinha = dados["carteirinha"]
+
+    usuarios[carteirinha] = dados
+
+    with open("usuarios.json", "w", encoding='utf-8') as arquivo:
+        json.dump(usuarios, arquivo, indent=4, ensure_ascii=False)
+
     return {
-        "mensagem": "Care+ Backend Online"
-    }
+        "status": "ok",
+        "mensagem": "Usuário salvo"
+    }   
 
-# rota login
-@app.post("/login")
-def login(usuario: Login):
+@app.get("/usuario/{carteirinha}")
+def buscar_usuario(carteirinha: str):
 
-    # simulação
-    if usuario.carteirinha == 123 and usuario.senha == "123@":
+    with open("usuarios.json", "r", encoding="utf-8") as arquivo:
+        usuarios = json.load(arquivo)
 
-        return {
-            "success": True,
-            "mensagem": "Login realizado com sucesso"
-        }
+    usuario = usuarios.get(carteirinha)
 
-    return {
-        "success": False,
-        "mensagem": "Carteirinha ou senha inválida"
-    }
+    return usuario
